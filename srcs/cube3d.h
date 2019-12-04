11d41f058f9ef1af5148532e8919331b1e3c709f
Paolo Ramella-Ratin <pramella@z1r11p1.le-101.fr>
2019-12-04 19:20:41 +0100

    adds first definition of structures

--- srcs/cube3d.h
+++ srcs/cube3d.h
@@ -6,9 +6,35 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/04 14:12:14 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/04 14:12:16 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/04 16:41:35 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#ifndef CUBE3D_H
+# define CUBE3D_H
 
+typedef struct	s_colour
+{
+	int			red;
+	int			green;
+	int			blue;
+}				t_colour;
+
+typedef struct	s_textures
+{
+	char		*north;
+	char		*south;
+	char		*west;
+	char		*east;
+}				t_textures;
+
+typedef struct	s_map
+{
+	char		*map;
+	t_textures	*text_ptr;
+	t_colour	*floor_ptr;
+	t_colour	*ceiling_ptr;
+}				t_map;
+
+#endif
