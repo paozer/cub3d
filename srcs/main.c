58bccd4cc9cb82b20a38ff62b0ff96ce72ef832b
Paolo Ramella-Ratin <pramella@z1r11p1.le-101.fr>
2019-12-05 16:30:47 +0100

    adds struct initialization

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,25 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/04 14:11:33 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/05 01:24:20 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/05 14:18:52 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "cube3d.h"
+#include "../includes/cub3d.h"
 
 int		main(int argc, char **argv)
 {
+	t_map *map;
+
+	if (!(map = malloc(sizeof(*map))))
+		return (-1);
+	if (!(map->text_ptr = malloc(sizeof(*map->text_ptr))))
+		return (-1);
+	if (!(map->floor_ptr = malloc(sizeof(*map->floor_ptr))))
+		return (-1);
+	if (!(map->ceiling_ptr = malloc(sizeof(*map->ceiling_ptr))))
+		return (-1);
+	ft_parsing(argv[1], map);
 	return (0);
 }
