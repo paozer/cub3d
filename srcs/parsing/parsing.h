374a905f71fa0e3fdb75c1c154604ad222a9462d
Paolo Ramella-Ratin <pramella@z1r11p1.le-101.fr>
2019-12-05 21:23:48 +0100

    norme

--- srcs/parsing/parsing.h
+++ srcs/parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/05 20:30:56 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/05 21:15:36 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/05 21:22:37 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,8 +14,8 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-#include "../cub3d.h"
-#include "../../libft/includes/libft.h"
+# include "../cub3d.h"
+# include "../../libft/includes/libft.h"
 
 int		ft_parsing(char *params, t_map **map);
 t_map	*ft_init_map(void);
