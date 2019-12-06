d769dee6f9532de2e6d40001c4f5af5522124f78
Paolo Ramella-Ratin <pramella@z1r10p2.le-101.fr>
2019-12-06 16:31:13 +0100

    adds unistd include

--- srcs/parsing/parsing.h
+++ srcs/parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/05 20:30:56 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/05 21:22:37 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/06 13:11:02 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,6 +16,7 @@
 
 # include "../cub3d.h"
 # include "../../libft/includes/libft.h"
+# include <unistd.h>
 
 int		ft_parsing(char *params, t_map **map);
 t_map	*ft_init_map(void);
