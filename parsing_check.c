920e7e6aec1e5ee16daa69aec55414824dd0022e
Paolo Ramella-Ratin <pramella@z1r10p2.le-101.fr>
2019-12-06 18:22:53 +0100

    changes nothing

--- parsing_check.c
+++ parsing_check.c
@@ -6,17 +6,19 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/06 16:22:56 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/06 17:59:01 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/06 18:15:21 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-static void ft_path_error()
-{}
+static void	ft_path_error(char *path)
+{
 
-void	ft_parsing_check(t_map **map)
+}
+
+void		ft_parsing_check(t_map **map)
 {
 
 }
