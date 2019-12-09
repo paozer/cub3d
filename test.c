0abd48cad0ea75e03ed115c458ff9ffc66ef6990
Paolo Ramella-Ratin <pramella@z1r3p4.le-101.fr>
2019-12-09 18:45:23 +0100

    adds test on open with spaces at end of file path

--- test.c
+++ test.c
@@ -6,14 +6,16 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/04 14:21:57 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/04 14:38:32 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/09 17:58:48 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "X11/includes/mlx.h"
 #include <stdlib.h>
+#include <fcntl.h>
 
+/*
 int		main(void)
 {
 	void *mlx_ptr;
@@ -25,4 +27,11 @@ int		main(void)
 		return (0);
 	mlx_loop(mlx_ptr);
 	return (0);
+}
+*/
+
+int		main(void)
+{
+	int fd = open("README.md", O_RDONLY);
+	printf("fd = %d\n", fd);
 }
