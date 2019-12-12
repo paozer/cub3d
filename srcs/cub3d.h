4746e22306a353616161856f1b7e3439bdcc779a
Paolo Ramella-Ratin <pramella@z1r3p4.le-101.fr>
2019-12-12 16:27:16 +0100

    adds basic implementation of raycasting algorithm

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/04 14:12:14 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/12 15:26:52 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/12 16:25:07 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,6 +24,8 @@
 # include <stdio.h>
 
 # define PLAYER map->player
+# define SCREEN map->screen
+# define RAY map->ray
 
 typedef struct	s_color
 {
@@ -50,8 +52,8 @@ typedef struct	s_res
 typedef struct	s_player
 {
 	int			set_flag;
-	double		pos_x;
-	double		pos_y;
+	double		x;
+	double		y;
 	double		dir_x;
 	double		dir_y;
 }				t_player;
@@ -59,26 +61,29 @@ typedef struct	s_player
 
 typedef struct	s_screen
 {
-	double		plane_x;
+	double		plane_x; // coordinates of plane perp to dir of player
 	double		plane_y;
-	double		cam_x;
+	double		cam_x; // position of ray on the plane/screen
 	double		cam_y;
 }				t_screen;
 
 typedef struct	s_ray
 {
-	double		ray_x;
-	double		ray_y;
-	double		ray_dir_x;
-	double		ray_dir_y;
-	double		map_x;
-	double		map_y;
-	double		side_dist_x;
+	double		x; // exact position of ray
+	double		y;
+	double		dir_x; // direction of the ray
+	double		dir_y;
+	int			map_x; // box of the map where ray is
+	int			map_y;
+	double		side_dist_x; // initial distance until x hit
 	double		side_dist_y;
-	double		delta_dist_x;
+	double		delta_dist_x; // distance between hits
 	double		delta_dist_y;
-	double		side_x;
-	double		side_y;
+	double		step_x; // what direction to step in
+	double		step_y;
+	int			side; // what kind of wall NS/EW was hit
+	int			hit; // did we hit a wall
+	double		wall_dist;
 }				t_ray;
 
 typedef struct	s_map
@@ -96,7 +101,9 @@ typedef struct	s_map
 	t_textures	*text_ptr;
 	t_color		*floor_ptr;
 	t_color		*ceiling_ptr;
-/* pour mlx stuff	t_mlx		*mlx; */
+	/* pour mlx stuff
+	t_mlx		*mlx;
+	*/
 }				t_map;
 
 
@@ -113,6 +120,9 @@ t_map			*ft_init_map(void);
 
 /* RAYCASTING */
 void			ft_raycasting(t_map *map);
+
+/* UTILITYS */
+double			ft_abs(double value);
 
 /* helper functions for debugging */
 
--- srcs/raycasting/raycasting_main.c
+++ srcs/raycasting/raycasting_main.c
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/12 01:38:04 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/12 03:30:39 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/12 16:24:04 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,8 +20,59 @@ void	ft_raycasting(t_map *map)
 	x = 0;
 	while (x < map->res_ptr->x)
 	{
-		PLAYER->cam_x = 2 * x / map->res_ptr->x - 1;
-		PLAYER->ray_dir_x = PLAYER->dir_x + PLAYER->plane_x * PLAYER->cam_x;
-		PLAYER->ray_dir_y = PLAYER->dir_y + PLAYER->plane_y * PLAYER->cam_y;
+		SCREEN->cam_x = 2 * x / map->res_ptr->x - 1;
+		RAY->dir_x = PLAYER->dir_x + SCREEN->plane_x * SCREEN->cam_x;
+		RAY->dir_y = PLAYER->dir_y + SCREEN->plane_y * SCREEN->cam_y;
+
+		RAY->map_x = (int)PLAYER->pos_x;
+		RAY->map_y = (int)PLAYER->pos_y;
+		RAY->delta_dist_x = ft_abs(1 / RAY->dir_x);
+		RAY->delta_dist_y = ft_abs(1 / RAY->dir_y);
+
+		if (RAY->dir_x < 0)
+		{
+			RAY->step_x = -1;
+			RAY->side_dist_x = (PLAYER->x - RAY->map_x) * RAY->delta_dist_x;
+		}
+		else
+		{
+			RAY->step_x = 1;
+			RAY->side_dist_x = (RAY->map_x + 1 - PLAYER->x) * RAY->delta_dist_x;
+		}
+		if (RAY->dir_y < 0)
+		{
+			RAY->step_y = -1;
+			RAY->side_dist_y = (PLAYER->y - RAY->map_y) * RAY->delta_dist_y;
+		}
+		else
+		{
+			RAY->step_y = 1;
+			RAY->side_dist_y = (RAY->map_y + 1 - PLAYER->y) * RAY->delta_dist_y;
+		}
+
+		RAY->hit = 0;
+		while (RAY->hit == 0)
+		{
+			if (RAY->side_dist_x < RAY->side_dist_y)
+			{
+				RAY->side_dist_x += RAY->delta_dist_x;
+				RAY->map_x += RAY->step_x;
+				RAY->side = 0;
+			}
+			else
+			{
+				RAY->side_dist_y += RAY->delta_dist_y;
+				RAY->map_y += RAY->step_y;
+				RAY->side = 1;
+			}
+			if (map->map_i[map_y][map_x] == 1)
+				RAY->hit = 1;
+		}
+		if (RAY->side == 0)
+			RAY->wall_dist = (RAY->map_x - PLAYER->x + (1 - RAY->step_x) / 2) / RAY->dir_x;
+		else
+			RAY->wall_dist = (RAY->map_y - PLAYER->y + (1 - RAY->step_y) / 2) / RAY->dir_y;
+
+		int line_h = (int) (map->map_height / RAY->wall_dist);
 	}
 }
