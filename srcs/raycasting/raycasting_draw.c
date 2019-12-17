a880b5ea9f30e21bd43069857252ceef38a0ab93
Paolo Ramella-Ratin <pramella@z1r11p1.le-101.fr>
2019-12-17 15:52:13 +0100

    adds selection of texture of the walls depending on which direction they face

--- srcs/raycasting/raycasting_draw.c
+++ srcs/raycasting/raycasting_draw.c
@@ -6,19 +6,23 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/14 17:50:21 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/16 17:01:45 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/17 15:49:32 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int			select_texture(t_map *map, int x, int y)
+int			select_texture(t_map *map)
 {
-	(void)map;
-	(void)x;
-	(void)y;
-	return (0xfa8080);
+	int i;
+
+	i = 0;
+	(RAY->side == 0 && RAY->dir_x > 0) ? (i = 0) : 0; // N
+	(RAY->side == 0 && RAY->dir_x < 0) ? (i = 1) : 0; // S
+	(RAY->side == 1 && RAY->dir_y < 0) ? (i = 2) : 0; // E
+	(RAY->side == 1 && RAY->dir_y > 0) ? (i = 3) : 0; // W
+	return (i);
 }
 
 void		draw(t_map *map, int x)
@@ -33,13 +37,14 @@ void		draw(t_map *map, int x)
 	draw_end = (RES->y + RAY->line_h) / 2;
 	draw_start = (draw_start > 0) ? draw_start : 0;
 	draw_end = (draw_end >= RES->y) ? RES->y - 1 : draw_end;
-	text_index = select_texture(map, x, y);
+	text_index = select_texture(map);
 	while (y < map->res_ptr->y)
 	{
 		if (y < draw_start)
 			IMG->buf[y * RES->x + x] = FLOOR->clr;
 		if (y >= draw_start && y < draw_end)
 			IMG->buf[y * RES->x + x] = 0xfa8080;
+//			IMG->buf[y * RES->x + x] = select_pixel(map, text_index, x, y)
 		if (y >= draw_end)
 			IMG->buf[y * RES->x + x] = CEILING->clr;
 		++y;
