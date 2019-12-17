d7e1b5da191314aa2718f46e32e3e38cc0dc7288
Paolo Ramella-Ratin <pramella@z1r11p1.le-101.fr>
2019-12-17 21:05:38 +0100

    adds wall textures

--- srcs/raycasting/raycasting_draw.c
+++ srcs/raycasting/raycasting_draw.c
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/14 17:50:21 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/17 15:49:32 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/17 21:02:31 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,6 +23,29 @@ int			select_texture(t_map *map)
 	(RAY->side == 1 && RAY->dir_y < 0) ? (i = 2) : 0; // E
 	(RAY->side == 1 && RAY->dir_y > 0) ? (i = 3) : 0; // W
 	return (i);
+}
+
+int			select_pixel(t_map *map, int i, int x, int y, int draw_end)
+{
+	int tex_x;
+	int	color;
+	double wall_x;
+
+	wall_x = (RAY->side) ? PLAYER->x + RAY->wall_dist * RAY->dir_x :
+			PLAYER->y + RAY->wall_dist * RAY->dir_y ;
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * (double)TEXT[i]->height);
+	((RAY->side == 0 && RAY->dir_x > 0) || (RAY->side == 1 && RAY->dir_y < 0))
+			? tex_x = TEXT[i]->width - tex_x - 1 : 0;
+	while (y < draw_end)
+	{
+		int d = y * 256 - RES->y * 128 + RAY->line_h * 128;
+		int tex_y = d * TEXT[i]->height / RAY->line_h / 256;
+		color = TEXT[i]->buf[tex_y * TEXT[i]->height + tex_x];
+		IMG->buf[y * RES->x + x] = color;
+		y++;
+	}
+	return (draw_end);
 }
 
 void		draw(t_map *map, int x)
@@ -43,8 +66,7 @@ void		draw(t_map *map, int x)
 		if (y < draw_start)
 			IMG->buf[y * RES->x + x] = FLOOR->clr;
 		if (y >= draw_start && y < draw_end)
-			IMG->buf[y * RES->x + x] = 0xfa8080;
-//			IMG->buf[y * RES->x + x] = select_pixel(map, text_index, x, y)
+			y = select_pixel(map, text_index, x, y, draw_end);
 		if (y >= draw_end)
 			IMG->buf[y * RES->x + x] = CEILING->clr;
 		++y;
