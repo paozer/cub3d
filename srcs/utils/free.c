98964f76a2ce21bceddec1986483cc6c98b30a61
paozer <p.ramellaratin@gmail.com>
2019-12-27 13:36:06 +0100

    renames files and puts private functions as static

---
+++ srcs/utils/free.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   free.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/19 22:57:17 by pramella     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/27 13:21:35 by paozer      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static void	free_extra(t_map *map, int flag)
+{
+	free(CEI_CLR);
+	free(FLO_CLR);
+	(flag == 1) ? free(SPR) : 0;
+	(flag == 1) ? free(MLX) : 0;
+	free(SCREEN);
+	free(RAY);
+	free(PLAYER);
+	free(RES);
+}
+
+static void	free_mlx(t_map *map, int flag)
+{
+	int i;
+
+	i = 0;
+	mlx_destroy_window(MLX->mlx_ptr, MLX->win_ptr);
+	free(MLX);
+	(flag == 3) ? 0 : free(MOVT);
+	while (i < 5)
+	{
+		free(TEXT[i]->img);
+		free(TEXT[i]->buf);
+		free(TEXT[i++]);
+	}
+	i = 0;
+	free(SPR->wall_dist);
+	i = 0;
+	while (SPR->arr[i])
+		free(SPR->arr[i++]);
+	free(SPR->arr);
+	free(SPR);
+	free(IMG->img);
+	free(IMG->buf);
+	free(IMG);
+}
+
+int			free_dummy(t_map *map)
+{
+	free_all(map, 2);
+	return (0);
+}
+
+void		free_all(t_map *map, int flag)
+{
+	int i;
+
+	i = 0;
+	if (flag != 0)
+	{
+		free_extra(map, flag);
+		while (i < 5)
+			free(TEXT_P[i++]);
+		i = 0;
+		while (map->map[i])
+			free(map->map[i++]);
+		free(map->map[i]);
+		free(map->map);
+		i = 0;
+		while (i < map->height)
+			free(map->map_i[i++]);
+		free(map->map_i);
+	}
+	(flag == 2 || flag == 3) ? free_mlx(map, flag) : 0;
+	free(map);
+	exit(0);
+}
--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -5,79 +5,78 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/12/19 22:57:17 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/22 06:57:19 by pramella    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/12/22 03:31:34 by pramella     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/24 15:03:31 by paozer      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		free_dummy(t_map *map)
+void	print_error(int flag, void *arg, t_map *map, int free_flag)
 {
-	free_all(map, 2);
-	return (0);
+	write(1, "Error\n", 6);
+	(flag == 0) ? ft_printf("Resolution is not valid.\n") : 1;
+	(flag == 1) ? ft_printf("Specified colors are not in the RGB range.\n") : 1;
+	(flag == 2) ? ft_printf("File \"%s\" does not exist.\n", arg) : 1;
+	(flag == 3) ? ft_printf("Path extension \"%s\" is not valid.\n", arg) : 1;
+	(flag == 4) ? ft_printf("Map character \"%c\" is not valid.\n",
+			*((char *)arg)) : 1;
+	(flag == 5) ? ft_printf("Map is not enclosed by walls in row %d.\n",
+			*((int *)arg) + 1) : 1;
+	(flag == 6) ? ft_printf("Multiple or no start positions for player.\n") : 1;
+	(flag == 7) ? ft_printf("No start position for player.\n") : 1;
+	(flag == 8) ? ft_printf("Could not allocate sufficient memory.\n") : 1;
+	(flag == 9) ? ft_printf("Multiple or missing specifications.\n") : 1;
+	free_all(map, free_flag);
 }
 
-void	free_all(t_map *map, int flag)
+char	*set_paths(char *line, t_map *map)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (line[i] && ft_isspace(line[i]))
+		++i;
+	str = ft_strdup(line + i);
+	++map->config_flag;
+	return (str);
+}
+
+int		ft_strlen_mod(char *str)
 {
 	int i;
+	int j;
 
 	i = 0;
-	if (flag != 0)
+	j = 0;
+	while (str[i])
 	{
-		free_extra(map, flag);
-		while (i < 5)
-			free(TEXT_P[i++]);
-		i = 0;
-		while (map->map[i])
-			free(map->map[i++]);
-		free(map->map[i]);
-		free(map->map);
-		i = 0;
-		while (i < map->height)
-			free(map->map_i[i++]);
-		free(map->map_i);
+		if (ft_isspace(str[i]) == 0)
+			++j;
+		++i;
 	}
-	(flag == 2 || flag == 3) ? free_mlx(map, flag) : 0;
-	free(map);
-	exit(0);
+	return (j);
 }
 
-void	free_extra(t_map *map, int flag)
+char	*ft_strdup_mod(char *str)
 {
-	free(CEI_CLR);
-	free(FLO_CLR);
-	(flag == 1) ? free(SPR) : 0;
-	(flag == 1) ? free(MLX) : 0;
-	free(SCREEN);
-	free(RAY);
-	free(PLAYER);
-	free(RES);
-}
-
-void	free_mlx(t_map *map, int flag)
-{
-	int i;
+	int		i;
+	int		j;
+	char	*s;
 
 	i = 0;
-	mlx_destroy_window(MLX->mlx_ptr, MLX->win_ptr);
-	free(MLX);
-	(flag == 3) ? 0 : free(MOVT);
-	while (i < 5)
+	j = 0;
+	if (!(s = malloc(sizeof(*s) * ft_strlen_mod(str) + 1)))
+		return (NULL);
+	while (str[i])
 	{
-		free(TEXT[i]->img);
-		free(TEXT[i]->buf);
-		free(TEXT[i++]);
+		if (ft_isspace(str[i]) == 0)
+			s[j++] = str[i];
+		++i;
 	}
-	i = 0;
-	free(SPR->wall_dist);
-	i = 0;
-	while (SPR->arr[i])
-		free(SPR->arr[i++]);
-	free(SPR->arr);
-	free(SPR);
-	free(IMG->img);
-	free(IMG->buf);
-	free(IMG);
+	s[j] = '\0';
+	free(str);
+	return (s);
 }
