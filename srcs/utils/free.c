3d7767cf940efd483bec6f64f99453a4c20e9044
Paolo Ramella-Ratin <pramella@z1r10p2.le-101.fr>
2020-01-13 01:22:39 +0100

    adds correct free of mlx images

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,7 +6,7 @@
 /*   By: pramella <pramella@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/19 22:57:17 by pramella     #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/12 20:44:21 by pramella    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/13 01:18:17 by pramella    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -31,26 +31,23 @@ static void	free_mlx(t_map *m, int flag)
 
 	if (m->save == 0)
 		mlx_destroy_window(m->mx->mlx_ptr, m->mx->win_ptr);
-	if (m->save == 1)
-		free(m->mx->mlx_ptr);
-	free(m->mx);
 	(flag == 3) ? 0 : free(m->mo);
 	i = 0;
 	while (i < 5)
 	{
-		free(m->t[i]->img);
-		free(m->t[i]->buf);
-		free(m->t[i++]);
+		mlx_destroy_image(m->mx->mlx_ptr, m->t[i]->img);
+		free(m->t[i]);
+		++i;
 	}
+	mlx_destroy_image(m->mx->mlx_ptr, m->i->img);
+	free(m->i);
+	free(m->mx);
 	free(m->sp->wall_dist);
 	i = 0;
 	while (m->sp->arr[i])
 		free(m->sp->arr[i++]);
 	free(m->sp->arr);
 	free(m->sp);
-	free(m->i->img);
-	free(m->i->buf);
-	free(m->i);
 }
 
 int			free_dummy(t_map *m)
@@ -75,11 +72,13 @@ void		free_all(t_map *m, int flag)
 		(m->map) ? free(m->map[i]) : 0;
 		free(m->map);
 		i = 0;
-		while (i < m->height)
+		printf("pute %d %p %p\n", m->height, m->map_i, m->map_i[0]);
+		while (m->map_i != NULL && i < m->height)
 			free(m->map_i[i++]);
 		free(m->map_i);
 	}
 	(flag == 2 || flag == 3) ? free_mlx(m, flag) : 0;
 	free(m);
+	while (1) ;
 	exit(0);
 }
